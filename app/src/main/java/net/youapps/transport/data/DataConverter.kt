063b3f4cfaaeb848f900a6894c1113ceb732698f
Bnyro <bnyro@tutanota.com>
2025-06-23 20:17:16 +0200

    fix: crash when location place is null/empty

--- app/src/main/java/net/youapps/transport/data/DataConverter.kt
+++ app/src/main/java/net/youapps/transport/data/DataConverter.kt
@@ -5,18 +5,24 @@ import de.schildbach.pte.dto.LocationType
 import net.youapps.transport.ProtobufLocation
 import net.youapps.transport.ProtobufRoute
 
-fun ProtobufLocation.toLocation(): Location = Location(LocationType.valueOf(type), id, name, place)
+fun ProtobufLocation.toLocation(): Location = Location(
+    LocationType.valueOf(type),
+    id,
+    place.takeIf { it.isNotEmpty() },
+    name.takeIf { it.isNotEmpty() }
+)
 
 fun Location.toProtobufLocation(): ProtobufLocation =
     ProtobufLocation.getDefaultInstance().toBuilder()
         .setType(type.name)
         .setId(id)
-        .setName(name)
-        .setPlace(place)
+        .setName(name.orEmpty())
+        .setPlace(place.orEmpty())
         .build()
 
-fun newProtobufRoute(origin: Location, destination: Location): ProtobufRoute = ProtobufRoute.getDefaultInstance()
-    .toBuilder()
-    .setOrigin(origin.toProtobufLocation())
-    .setDestination(destination.toProtobufLocation())
-    .build()
+fun newProtobufRoute(origin: Location, destination: Location): ProtobufRoute =
+    ProtobufRoute.getDefaultInstance()
+        .toBuilder()
+        .setOrigin(origin.toProtobufLocation())
+        .setDestination(destination.toProtobufLocation())
+        .build()
