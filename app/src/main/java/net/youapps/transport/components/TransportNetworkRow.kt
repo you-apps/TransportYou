f8afe9fde0009ee620d237c0a6c5bd70e1aa3b33
Bnyro <bnyro@tutanota.com>
2025-06-23 21:37:19 +0200

    feat: highlight currently selected transport network

--- app/src/main/java/net/youapps/transport/components/TransportNetworkRow.kt
+++ app/src/main/java/net/youapps/transport/components/TransportNetworkRow.kt
@@ -1,11 +1,16 @@
 package net.youapps.transport.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import net.youapps.transport.data.TransportNetwork
@@ -16,10 +21,16 @@ fun TransportNetworkRow(network: TransportNetwork, isSelected: Boolean, onClick:
         modifier = Modifier
             .fillMaxWidth()
             .clickable {
-            onClick.invoke()
-        }
-            .padding(horizontal = 10.dp, vertical = 6.dp)
+                onClick.invoke()
+            }
+            .padding(horizontal = 10.dp, vertical = 6.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(text = network.name)
+
+        if (isSelected) {
+            Icon(imageVector = Icons.Default.Check, contentDescription = null)
+        }
     }
 }
