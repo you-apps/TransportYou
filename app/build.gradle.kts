cf4627b49d7bba57500069987a8d5e122057cef6
Bnyro <bnyro@tutanota.com>
2025-09-30 12:09:43 +0200

    chore: bump version to v2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.youapps.transport"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
